a390a97e6f61ae31507a2bb8c3a5855404b6e9df
phpcheatsheet <reikanov@yandex.ru>
2016-12-24 15:38:59 +0300

    small changes

--- js/main.js
+++ js/main.js
@@ -1,1 +1,1 @@
-$(document).ready(function(){$('.acc-title').on('click',function(e){e.preventDefault();$(this).next().slideToggle();});console.log('В консольку тоже любишь заглядывать, да?')});
+$(document).ready(function(){$('.acc-title').on('click',function(e){e.preventDefault();$(this).next().slideToggle();});console.log('В консольку тоже любишь заглядывать? Программист что ли?')});
